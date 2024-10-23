a29e80984d679fb4012ac927826bed148a0e8d4b
Umaima Khan <umaima.khan@my.tccd.edu>
2024-10-23 13:43:26 -0500

    Assignment Corrections lab3

--- labs/Lab3/FallingDistance/FallingDistance.cpp
+++ labs/Lab3/FallingDistance/FallingDistance.cpp
@@ -7,14 +7,22 @@
 #include <iomanip> // for std::setw and std::setprecision
 using namespace std;
 
-// Story 1: Function to display program information
+// Story 1: This function displays programming information
+// Function: displayProgramInfo
+// Purpose: Displays the program's title, author, and course information.
+// Parameters: None
+// Returns: Nothing (void)
 void displayProgramInfo() {
     cout << "Program Title: Falling Distance Calculator\n";
     cout << "Name: Umaima Khan\n";
     cout << "Course: COSC 1436 - Fall 2024\n";
 }
 
-// Story 2: Function to prompt the user for falling time (1-60 seconds)
+// Story 2: This function prompts the user for falling time ie, (1-60 seconds)
+// Function: getFallingTime
+// Purpose: Prompt the user to enter a valid falling time between 1 and 60 seconds.
+// Parameters: None
+// Returns: An integer representing the falling time.
 int getFallingTime() {
     int time;
     do {
@@ -28,13 +36,21 @@ int getFallingTime() {
     return time;
 }
 
-// Story 3: Function to calculate the falling distance based on time (in seconds)
+// Story 3: This is a function to calculate the falling distance based on time (in seconds)
+// Function: calculateFallingDistance
+// Purpose: Calculates the falling distance using the formula 0.5 * g * t^2, where g is gravity and t is time.
+// Parameters: An integer representing the time in seconds.
+// Returns: A double representing the calculated falling distance in meters.
 double calculateFallingDistance(int time) {
     const double gravity = 9.8; // Gravitational constant in meters per second squared
     return 0.5 * gravity * time * time; // Distance = 0.5 * g * t^2
 }
 
-// Story 4 & 6: Display the falling distance table with correct units (meters or feet)
+// Story 4 & 6: To display the falling distance table with correct units (meters or feet)
+// Function: displayFallingTable
+// Purpose: This Function displays a table of falling distances for each second, either in meters or feet.
+// Parameters: An integer for the falling time and a boolean for whether the distances should be in meters or feet.
+// Returns: Nothing (void)
 void displayFallingTable(int time, bool inMeters) {
     // Display the table header
     cout << "\nSeconds      Distance\n";
@@ -57,7 +73,11 @@ void displayFallingTable(int time, bool inMeters) {
     }
 }
 
-// Story 5: Function to allow user to choose the units (meters or feet)
+// Story 5: This function allows the user to choose the units (meters or feet)
+// Function: getUnitsChoice
+// Purpose: Prompts the user to choose the unit for displaying distances (meters or feet).
+// Parameters: None
+// Returns: A boolean value; true if the user chooses meters, false for feet.
 bool getUnitsChoice() {
     char unit;
     do {
@@ -72,7 +92,11 @@ bool getUnitsChoice() {
     return (unit == 'm'); // Return true if meters is chosen, false for feet
 }
 
-// Main function (entry point of the program)
+// This is the main function (entry point of the program)
+// Function: main
+// Purpose: Coordinates the program's flow by calling the other functions.
+// Parameters: None
+// Returns: An integer value indicating successful program termination (0).
 int main() {
     // Display program information (Story 1)
     displayProgramInfo();
